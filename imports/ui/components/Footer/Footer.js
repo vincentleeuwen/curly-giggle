0150175216f35f2cfbf1b655d99b6e45036d231c
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-09-08 16:37:55 +0200

    update footer

--- imports/ui/components/Footer/Footer.js
+++ imports/ui/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { year } from '@cleverbeagle/dates';
+import { year, monthDayYear } from '@cleverbeagle/dates';
 import { Link } from 'react-router-dom';
 import { Grid } from 'react-bootstrap';
 
@@ -10,10 +10,15 @@ const copyrightYear = () => {
   return currentYear === '2017' ? '2017' : `2017-${currentYear}`;
 };
 
+const today = () => {
+  const currentDate = monthDayYear();
+  return `${currentDate}`;
+};
+
 const Footer = () => (
   <div className="Footer">
     <Grid>
-      <p className="pull-left">&copy; {copyrightYear()} Application Name</p>
+      <p className="pull-left">{today()} - Vincent van Leeuwen</p>
       <ul className="pull-right">
         <li><Link to="/terms">Terms<span className="hidden-xs"> of Service</span></Link></li>
         <li><Link to="/privacy">Privacy<span className="hidden-xs"> Policy</span></Link></li>
